80b50d52ea7c05df4a5859424f11900eccce5521
Swarit172 <114414388+Swarit172@users.noreply.github.com>
2022-11-06 00:41:43 +0530

    Time: 25 ms (99.22%), Space: 14.2 MB (53.46%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.py
+++ 0035-search-insert-position/0035-search-insert-position.py
@@ -11,13 +11,13 @@ class Solution(object):
             return high+1
         
         while(low<=high):
-            mid=(low+high)/2
-            if (nums[mid]<target):
+            mid=(low+high)//2
+            if (nums[mid]==target):
+                return mid
+                break
+            elif (nums[mid]<target):
                 low=mid+1
             else:
-                if (nums[mid]==target and nums[mid-1]!=target):
-                    return mid
-                else:
-                    high=mid-1
+                high=mid-1
         return low
         
